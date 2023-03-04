42e65fd656036a2ce5cb1738c808e100311ea970
Aleksey <kozlovav78@mail.ru>
2023-03-04 15:38:53 +0300

    Добавлена папка task с решением задачи

---
+++ task/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+using static System.Console;
+
+string[] GetArray()
+{
+    Write("Введите значения массива через пробел: ");
+    return ReadLine().Split(" ");
+}
+
+string[] ResultArray(string[] array)
+{
+    int count = 0;
+    string[] result = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    return result;
+}
+
+string[] array = GetArray();
+string[] result = ResultArray(array);
+
+Console.WriteLine("Массив из строк, длина которых меньше, либо равна 3 символам:");
+WriteLine($"[{string.Join(" ", result)}]");
